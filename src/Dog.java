242eb960a571fd71da01c6f163224a945ffdc6da
Teo2525 <97795953+Teo2525@users.noreply.github.com>
2022-05-19 11:48:13 +0400

    Update Dog.java
    added 3 lines of code Dog3, 4, 5

--- src/Dog.java
+++ src/Dog.java
@@ -2,5 +2,8 @@ public class Dog {
     public static void main(String[] args) {
         System.out.println("this is a dog");
         System.out.println("this is dog2");
+        System.out.println("this is dog3");
+        System.out.println("this is dog4");
+        System.out.println("this is dog5");
     }
 }
